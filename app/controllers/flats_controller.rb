c68fdde2b5568a583d1a0719dee5feae22e8eb55
fabiosimoesds <fabio.simoesds@gmail.com>
2022-08-17 21:07:12 +0100

    Added imgs to the flats and add bootstrap

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,47 @@
+class FlatsController < ApplicationController
+  before_action :set_flats, only: %i[show edit update destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
+  end
+
+  private
+
+  def set_flats
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :number_of_guests, :price_per_night, :description)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Deleting all Flats'
+Flat.destroy_all
+
+puts 'Adding 3 Flats to the Database'
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  url_img: 'https://images.unsplash.com/photo-1529408632839-a54952c491e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+)
+
+Flat.create!(
+  name: 'Cabin Woods Paris',
+  address: '198 Flower Gardens Paris W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 60,
+  number_of_guests: 4,
+  url_img: 'https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+)
+
+Flat.create!(
+  name: 'Entire House',
+  address: '158 Rua das Portas, Portugal',
+  description: 'Enormous House with garden. Four double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 150,
+  number_of_guests: 8,
+  url_img: 'https://images.unsplash.com/photo-1655986404567-7af4e456b8ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+)
+
+puts 'Flats Created'
